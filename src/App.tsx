492a090410a0108b09d563ca95d0ff52c7ee6f30
Christopher Hallett <chrisjameshal@gmail.com>
2022-01-25 17:24:38 +0000

    Add full details to workout cards (unformatted)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import axios from "axios";
-import { useCallback, useEffect, useState } from "react";
-import { MuscleType } from "./types/MuscleType";
-import { ExerciseImageType } from "./types/ExerciseImageType";
-import { ExerciseType } from "./types/ExerciseType";
-import { getMuscleName } from "./utils/getMuscleName";
+// import axios from "axios";
+// import { useCallback, useEffect, useState } from "react";
+// import { MuscleType } from "./types/MuscleType";
+// import { ExerciseImageType } from "./types/ExerciseImageType";
+// import { ExerciseType } from "./types/ExerciseType";
+// import { getMuscleName } from "./utils/getMuscleName";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { getExerciseImage } from "./utils/getExerciseImage";
+// import { getExerciseImage } from "./utils/getExerciseImage";
 import { NavigationBar } from "./components/NavigationBar";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { StatsPage } from "./components/StatsPage";
@@ -15,54 +15,54 @@ import { config } from "dotenv";
 
 config();
 
-const baseURL = process.env.REACT_APP_API_BASE;
+// const baseURL = process.env.REACT_APP_API_BASE;
 
 function App(): JSX.Element {
-  const [exerciseList, setExerciseList] = useState<ExerciseType[]>([]);
-  const [imageList, setImageList] = useState<ExerciseImageType[]>([]);
-  const [muscleList, setMuscleList] = useState<MuscleType[]>([]);
+  // const [exerciseList, setExerciseList] = useState<ExerciseType[]>([]);
+  // const [imageList, setImageList] = useState<ExerciseImageType[]>([]);
+  // const [muscleList, setMuscleList] = useState<MuscleType[]>([]);
 
-  const getExercises = useCallback(async () => {
-    try {
-      // Custom link obtained from seeing how many exercises are in database (419) and limiting the search to those
-      const res = await axios.get(
-        "https://wger.de/api/v2/exercise/?limit=419&offset=0"
-      );
-      const exerciseResults: ExerciseType[] = res.data.results;
-      const filteredResults: ExerciseType[] = exerciseResults.filter(
-        (exercise) => exercise.language === 2
-      );
-      setExerciseList(filteredResults);
-      console.log(filteredResults);
-    } catch (err) {
-      console.log(err);
-    }
-  }, []);
+  // const getExercises = useCallback(async () => {
+  //   try {
+  //     // Custom link obtained from seeing how many exercises are in database (419) and limiting the search to those
+  //     const res = await axios.get(
+  //       "https://wger.de/api/v2/exercise/?limit=419&offset=0"
+  //     );
+  //     const exerciseResults: ExerciseType[] = res.data.results;
+  //     const filteredResults: ExerciseType[] = exerciseResults.filter(
+  //       (exercise) => exercise.language === 2
+  //     );
+  //     setExerciseList(filteredResults);
+  //     console.log(filteredResults);
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // }, []);
 
-  const getImages = useCallback(async () => {
-    try {
-      // Custom link obtained from seeing how many images are in database (118) and limiting the search to those
-      const res = await axios.get(
-        "https://wger.de/api/v2/exerciseimage/?limit=118&offset=0"
-      );
-      const imageResults: ExerciseImageType[] = res.data.results;
-      setImageList(imageResults);
-      console.log(imageResults);
-    } catch (err) {
-      console.log(err);
-    }
-  }, []);
+  // const getImages = useCallback(async () => {
+  //   try {
+  //     // Custom link obtained from seeing how many images are in database (118) and limiting the search to those
+  //     const res = await axios.get(
+  //       "https://wger.de/api/v2/exerciseimage/?limit=118&offset=0"
+  //     );
+  //     const imageResults: ExerciseImageType[] = res.data.results;
+  //     setImageList(imageResults);
+  //     console.log(imageResults);
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // }, []);
 
-  const getMuscles = useCallback(async () => {
-    try {
-      const res = await axios.get("https://wger.de/api/v2/muscle/");
-      const muscleResults: MuscleType[] = res.data.results;
-      setMuscleList(muscleResults);
-      console.log(muscleResults);
-    } catch (err) {
-      console.log(err);
-    }
-  }, []);
+  // const getMuscles = useCallback(async () => {
+  //   try {
+  //     const res = await axios.get("https://wger.de/api/v2/muscle/");
+  //     const muscleResults: MuscleType[] = res.data.results;
+  //     setMuscleList(muscleResults);
+  //     console.log(muscleResults);
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // }, []);
 
   // const postSets = useCallback(async () => {
   //   const data = {
@@ -87,16 +87,16 @@ function App(): JSX.Element {
   //   }
   // }, []);
 
-  useEffect(() => {
-    getExercises();
-    getImages();
-    getMuscles();
-  }, [getExercises, getImages, getMuscles]);
+  // useEffect(() => {
+  //   getExercises();
+  //   getImages();
+  //   getMuscles();
+  // }, [getExercises, getImages, getMuscles]);
 
   return (
     <>
       <NavigationBar />
-      {/* Switch chooses just one page to access */}
+      {/* Define the files that should be accessed when a given path is accessed */}
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,8 +1,9 @@
 import axios from "axios";
 import { useCallback, useEffect, useState } from "react";
-import { MuscleType } from "../../types/MuscleType";
 import "./HomePage.css";
 import { config } from "dotenv";
+import { FaWeightHanging, FaClock } from "react-icons/fa";
+import { GiWeightLiftingUp } from "react-icons/gi";
 
 config();
 
@@ -19,8 +20,25 @@ interface WorkoutType {
   exercises: number;
 }
 
+interface SetType {
+  set_id: number;
+  workout_id: number;
+  name: string;
+  weight: number;
+  reps: number;
+}
+
+interface BestSetType {
+  workout_id: number;
+  name: string;
+  weight: number;
+  reps: number;
+}
+
 export function HomePage(): JSX.Element {
   const [workoutsList, setWorkoutsList] = useState<WorkoutType[]>([]);
+  const [setsList, setSetsList] = useState<SetType[]>([]);
+  const [bestSetsList, setBestSetsList] = useState<SetType[]>([]);
 
   const getWorkouts = useCallback(async () => {
     try {
@@ -33,9 +51,35 @@ export function HomePage(): JSX.Element {
     }
   }, []);
 
+  const getSets = useCallback(async () => {
+    console.log("trying to getSets");
+    try {
+      const res = await axios.get(`${baseURL}/sets`);
+      const setResults = res.data.data;
+      setSetsList(setResults);
+      console.log({ setResults });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
+  const getBestSets = useCallback(async () => {
+    console.log("trying to getSets");
+    try {
+      const res = await axios.get(`${baseURL}/sets/best`);
+      const setResults = res.data.data;
+      setBestSetsList(setResults);
+      console.log({ setResults });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
   useEffect(() => {
     getWorkouts();
-  }, [getWorkouts]);
+    getSets();
+    getBestSets();
+  }, [getWorkouts, getSets, getBestSets]);
 
   return (
     <div className="home-page">
@@ -55,9 +99,20 @@ export function HomePage(): JSX.Element {
         </div>
       </div>
       {/* List of past workouts */}
-      <div className="workouts-list">
+      <div className="workout-list">
         {workoutsList.map((workout, index) => {
-          return <WorkoutCard key={index} workout={workout} />;
+          return (
+            <WorkoutCard
+              key={index}
+              workout={workout}
+              setsList={setsList.filter(
+                (set) => set.workout_id === workout.workout_id
+              )}
+              bestSetsList={bestSetsList.filter(
+                (set) => set.workout_id === workout.workout_id
+              )}
+            />
+          );
         })}
       </div>
     </div>
@@ -66,21 +121,51 @@ export function HomePage(): JSX.Element {
 
 interface WorkoutCardProps {
   workout: WorkoutType;
+  setsList: SetType[];
+  bestSetsList: BestSetType[];
 }
+
 function WorkoutCard(props: WorkoutCardProps): JSX.Element {
   return (
     <div
       className="workout-card"
       id={`workout-card-${props.workout.workout_id}`}
     >
-      <div className="workout-card-header">
+      <div className="workout-card-row1">
         <h2 className="workout-card-title">{props.workout.title}</h2>
-        <h3>{props.workout.weight_lifted}</h3>
-        <h3>{props.workout.exercises}</h3>
+        <h3 className="workout-card-weight-lifted">
+          <FaWeightHanging />
+          {props.workout.weight_lifted}
+        </h3>
+        <h3 className="workout-card-duration">
+          <FaClock />
+          {props.workout.duration_mins}
+        </h3>
+        <h3 className="workout-card-exercises">
+          <GiWeightLiftingUp />
+          {props.workout.exercises}
+        </h3>
       </div>
-
-      <h3 className="workout-card-day">{props.workout.day}</h3>
-      <h4 className="workout-card-date">{props.workout.date}</h4>
+      <div className="workout-card-row2">
+        <h3 className="workout-card-day">{props.workout.day}</h3>
+        <h4 className="workout-card-date">{props.workout.date}</h4>
+      </div>
+      <hr />
+      <div className="workout-card-body">
+        <h5 className="workout-card-best-sets-title">Highlights</h5>
+        {props.bestSetsList.slice(0, 3).map((set, index) => {
+          return (
+            <div
+              key={`best-set-div-${index}`}
+              className="workout-card-best-set"
+            >
+              <h6 key={`best-set-name-${index}`}>{set.name}</h6>
+              <h6 key={`best-set-weight-${index}`}>{set.weight} Kg</h6>
+              <h6 key={`best-set-reps-${index}`}>{set.reps}x</h6>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
